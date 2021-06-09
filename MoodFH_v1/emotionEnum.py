ebbc90935dd20c224f3c2bba60ef2e8f5d3de330
lisi-anz <67186528+lisi-anz@users.noreply.github.com>
2021-06-09 18:14:23 +0200

    Enum angepasst

--- MoodFH_v1/emotionEnum.py
+++ MoodFH_v1/emotionEnum.py
@@ -3,10 +3,10 @@ from enum import Enum
 # Enum fuer verschiedene Emotionen 
 class Emotion(Enum):
     NONE = 0
-    NEUTRAL = 1
-    HAPPY = 2
-    SAD = 3
-    ANGRY = 4
-    DISGUSTED = 5
-    FEARFUL = 6
+    ANGRY = 1
+    DISGUSTED = 2
+    FEARFUL = 3
+    HAPPY = 4
+    NEUTRAL = 5
+    SAD = 6
     SURPRISED = 7
